8df41c737de704faad4653deace6c898827780a6
Palvaneh <Palvaneh@Ideal.local>
2023-07-10 11:35:52 +0330

    add post query to show ontime in home page with useQueryClient hook

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,12 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient,  } from "@tanstack/react-query";
 import React from "react";
 import { createPost } from "../api/posts";
 import PostFrom from "./PostFrom";
 import {v4 as uuidv4} from 'uuid'
 
 const AddPost = () => {
+  const queryClient = useQueryClient()
   const createPostMutation = useMutation({
     mutationFn: createPost,
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey:["posts"]})
+    }
   });
 
   const handleAddPost = (post)=>{
